7bb61daa74437dbd9340a92493d2126411844501
Nobita <aurelio.ogliari@gmail.com>
2014-11-19 21:18:59 +0000

    add grunt & sass

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass:base']
+      }
+    },
+    sass: {
+      base: {
+        files: {
+          'src/styles/style.css': 'src/styles/style.scss'          
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.registerTask('watch-css', ['watch:css']);
+  grunt.registerTask('default', []);
+};
