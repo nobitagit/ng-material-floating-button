c446aa5543f88e836141d422a15eba660113d1dc
Nobita <aurelio.ogliari@gmail.com>
2014-12-03 20:57:34 +0000

    gh-pages task

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,13 +15,31 @@ module.exports = function(grunt) {
           'src/styles/style.css': 'src/styles/style.scss'          
         }
       }
-    }
+    },
+
+    livePages: ['index.html', 'index.css', 'showcase.html', 'styles/*.css'],
+    'gh-pages': {
+      options: {
+        base: 'src',
+      },
+      'live': {
+        src: ['<%= livePages %>'] 
+      },
+      'check': {
+        options: {
+          push: false
+        },
+        src: ['<%= livePages %>']      
+      }
+    }    
   });
 
   grunt.loadNpmTasks('grunt-gh-pages');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
 
+  grunt.registerTask('live', ['gh-pages:live']);
+  grunt.registerTask('livecheck', ['gh-pages:check']);
   grunt.registerTask('watch-css', ['watch:css']);
   grunt.registerTask('default', []);
 };
