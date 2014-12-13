b66241a86470d15e3e609c802368b0dca8c2a627
Nobita <aurelio.ogliari@gmail.com>
2014-12-13 12:13:11 +0000

    setup main and child directives

--- src/directive.js
+++ src/directive.js
@@ -3,6 +3,36 @@ var mfb = angular.module('ng-mfb', []);
 mfb.directive('mfbMenu', [function(){
   return {
     restrict: 'EA',
-    template: '<div>test template</div>'
+    transclude: true,
+    replace: true,
+    scope: {
+      position: '@',
+      effect: '@'
+    },
+    link: function(scope, elem, attrs){
+      scope.pos = attrs.position;
+      scope.eff = attrs.effect;
+    },
+    template: '<ul class="mfb-component--{{pos}} mfb-{{eff}}" ng-transclude></ul>'    
+  };
+}]);
+
+mfb.directive('mfbMainButton', [function(){
+  return {
+    require: '^mfbMenu',
+    restrict: 'EA',
+    transclude: true,  
+    replace: true,
+    template: '<li>test</li>'
+  };
+}]);
+
+mfb.directive('mfbButton', [function(){
+  return {
+    require: '^mfbMenu',
+    restrict: 'EA',
+    transclude: true,  
+    replace: true,
+    template: '<li ng-transclude></li>'
   };
 }]);
--- test/unit.spec.js
+++ test/unit.spec.js
@@ -1,7 +1,14 @@
 describe('ng-mfb', function() {
 
+  var $compile,
+      $rootScope;
+
   beforeEach(module('ng-mfb'));
 
-  
+  beforeEach(inject(function(_$compile_, _$rootScope_){
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+  }));
+
 
 });
