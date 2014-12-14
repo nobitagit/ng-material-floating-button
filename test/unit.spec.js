bbc08ff82c6dd0e9810d613172aa2d23a308a99c
Nobita <aurelio.ogliari@gmail.com>
2014-12-14 18:26:51 +0000

    setup basic testing templates

--- test/unit.spec.js
+++ test/unit.spec.js
@@ -10,5 +10,23 @@ describe('ng-mfb', function() {
     $rootScope = _$rootScope_;
   }));
 
+  function compile( template ){
+    var node = $compile('<div>' + template + '</div>')($rootScope);
+    return node.contents();
+  }
+
+  it('should transclude the div element properly', function(){
+    //expect(1+3).toBe(9);
+    var tpl = '<div mfb-menu  position="tr" effect="zoomin" label="hover here" active-icon="ion-edit" resting-icon="ion-plus-round">' +
+              '  <a mfb-button icon="icon-name" label="label-text" ng-repeat="button in buttons"></a>' +
+              '</div>'
+    var node = compile(tpl);
+    $rootScope.$digest();
+
+    console.log(node.children())
+    console.log(node.children().length )
+    expect(node[0].nodeType).toEqual(1);
+    expect(node[1]).toBeUndefined();    
+  });
 
 });
